d841be626b7dc3dac30fc664cd7b0d281134723f
Mithilesh Kumar Singh <shadowfightbeast@gmail.com>
2026-05-02 23:41:27 +0530

    final code fix

--- main.go
+++ main.go
@@ -3,15 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	// Intentionally  messy  ssssds cxdes for the bot to review
 	var x int = 10
 	if x > 5 {
 		fmt.Println("X is greater than 5")
 	}
 
-	var API_KEY = "1234"
-	var GOOGLE_API_KEY = "1234"
-
-	var data *string
+	str := "hello"
+	data := &str
 	fmt.Println(*data)
 }
